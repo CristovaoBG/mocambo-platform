f43de5ce180cc795f280abb022214f82dff9c931
Cristovao <cristovao@live.com>
2024-07-09 18:00:29 -0300

    fix url and build

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/mocambo-platform/dist/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/mocambo-platform/' : '/'
 }
