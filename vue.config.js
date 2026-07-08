ab5ce9504b424a5c66dd6bab52cf3d98157686a0
CristovaoBG <cristovao@live.com>
2026-07-08 16:04:42 -0300

    feat: inject SEO metadata into HTML at build time

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
+const seo = require('./src/content/seo.cjs')
+
+const htmlSeo = {
+  title: seo.title,
+  description: seo.description,
+  canonicalUrl: seo.url,
+  lang: seo.lang,
+  locale: seo.locale,
+  ogTitle: seo.ogTitle,
+  ogDescription: seo.ogDescription,
+  ogImageUrl: seo.ogImageUrl,
+  twitterCard: seo.twitterCard,
+  jsonLd: JSON.stringify(seo.getJsonLd()),
+}
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = seo.title
+      args[0].seo = htmlSeo
+
+      return args
+    })
+  },
 })
